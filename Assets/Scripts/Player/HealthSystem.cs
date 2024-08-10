142754582cd7c15312d655b3bebacbc1ac6f5783
Faris I <73676744+Faris122@users.noreply.github.com>
2024-08-10 23:27:53 +0800

    ---

---
+++ Assets/Scripts/Player/HealthSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Image healthBar;  // The health bar UI element
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            // Handle player death here
+        }
+        UpdateHealthBar();
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        UpdateHealthBar();
+    }
+
+    void UpdateHealthBar()
+    {
+        healthBar.fillAmount = (float)currentHealth / maxHealth;
+    }
+}
