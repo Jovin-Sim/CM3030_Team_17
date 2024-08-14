16adfd07170df95f7030e08bc3ac93ec0dd1c3f8
Faris I <73676744+Faris122@users.noreply.github.com>
2024-08-14 22:24:23 +0800

    ---

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject instructionsPanel;
+
+    public void StartGame()
+    {
+        // Load the main game scene (replace "GameScene" with your actual scene name)
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void ShowInstructions()
+    {
+        instructionsPanel.SetActive(true);
+    }
+
+    public void CloseInstructions()
+    {
+        instructionsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
