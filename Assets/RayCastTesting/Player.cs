dfabeb84542c0e29ce89d7169c13fda699c0c3e5
Benjamin <benjaminchhia@gmail.com>
2024-09-04 18:23:42 +0800

    I just realised i didnt push and commit
    so sorry guys

---
+++ Assets/RayCastTesting/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    [SerializeField]
+    private float _speed;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        LookAtMouse();
+        Move();
+    }
+
+    private void LookAtMouse()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.up = mousePos - new Vector2(transform.position.x, transform.position.y);
+    }
+
+    private void Move()
+    {
+        var input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _rigidbody.velocity = input.normalized * _speed;
+    }
+}
